2930b3d9fa31f6a483c755e557117c0dbe7d9df5
Kwesi20000 <hendrickskumi@yahoo.com>
2023-10-18 12:32:12 -0700

    done

---
+++ README.md
@@ -0,0 +1,1 @@
+Our Monty project readme
---
+++ main.c
@@ -0,0 +1,84 @@
+#include "monty.h"
+stack_t *head = NULL;
+
+/**
+ * main - entry point
+ * @argc: The argument count
+ * @argv: The argument vector
+ * Return: Always 0 on Success
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	open_file(argv[1]);
+	free_nodes();
+	return (0);
+}
+
+/**
+ * create_node - Function creates a node.
+ * @i: The number to go inside the node.
+ * Return: Return a pointer to the node on success. Otherwise NULL.
+ */
+stack_t *create_node(int n)
+{
+	stack_t *node;
+
+	node = malloc(sizeof(stack_t));
+	if (node == NULL)
+		error(4);
+	node->next = NULL;
+	node->prev = NULL;
+	node->n = n;
+	return (node);
+}
+
+/**
+ * free_nodes - Function frees nodes in the stack.
+ * Return: Nothing
+ */
+void free_nodes(void)
+{
+	stack_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
+
+
+/**
+ * add_to_queue - Function adds a node to the queue.
+ * @new_node: A pointer to the new node.
+ * @ln: line number of the opcode.
+ */
+void add_to_queue(stack_t **new_node, __attribute__((unused))unsigned int ln)
+{
+	stack_t *temp;
+
+	if (new_node == NULL || *new_node == NULL)
+		exit(EXIT_FAILURE);
+	if (head == NULL)
+	{
+		head = *new_node;
+		return;
+	}
+	temp = head;
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	temp->next = *new_node;
+	(*new_node)->prev = temp;
+
+}
