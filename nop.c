613a4953bbda3cc2f9bd42ccb355cfb664f2f808
Kwesi20000 <hendrickskumi@yahoo.com>
2023-10-19 15:23:05 -0700

    done

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ *f_nop - function does nothing
+ *@head: stack head
+ *@line_num: line_number
+ *Return: Nothing
+*/
+void f_nop(stack_t **head, unsigned int line_num)
+{
+	(void) line_num;
+	(void) head;
+}
