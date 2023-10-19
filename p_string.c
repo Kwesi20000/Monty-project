dd397cf1ff6987ae19752e99a9a641c5f3af54e5
Kwesi20000 <hendrickskumi@yahoo.com>
2023-10-19 15:10:25 -0700

    done

---
+++ p_string.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+/**
+ * f_pstring - funtion prints the string starting at the top of the stack
+ * followed by a new
+ * @head: stack head
+ * @line_num: line_number
+ * Return: Nothing.
+*/
+void f_pstr(stack_t **head, unsigned int line_num)
+{
+	stack_t *h;
+	(void)line_num;
+
+	h = *head;
+	while (h)
+	{
+		if (h->n > 127 || h->n <= 0)
+		{
+			break;
+		}
+		printf("%c", h->n);
+		h = h->next;
+	}
+	printf("\n");
+}
